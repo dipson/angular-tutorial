7642268af87aec77783ba1e9a6e8d68a0252349c
dipson <david.ipsen@gmail.com>
2013-06-01 20:32:12 -0400

    Changes for dynamic list of phones. Unit test added.

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -4,4 +4,13 @@
 
 describe('controllers', function() {
 
+	describe('PhoneListCtrl', function() {
+		it('should create "phones" model with 3 phones', function() {
+			var scope = {},
+			ctrl = new PhoneListCtrl(scope);
+
+			expect(scope.phones.length).toBe(3);
+		})
+	});
+
 });
